eecb343a8ef9c2694919a01fc6a4cd5d8c065938
Palaz <zoltan.palatinus@inteliart.hu>
2018-06-16 09:25:20 +0200

    display the weather

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Init weather bject
 const weather = new Weather('Boston', 'MA');
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM Load
 document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather(){
     weather.getWeather()
     .then(results => {
-        console.log(results);
+        ui.paint(results);
     })
     .catch(err => console.log(err));
 }
